06a6d4b89dcbcbe26ae0ef67eeb72527f15e6aba
Rob Ferguson <rjferguson21@gmail.com>
2023-07-27 20:05:02 -0500

    chore: remove k3d dir, use dubbd for testing

--- k3d/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-k3d cluster delete -c k3d.yaml
-k3d cluster create -c k3d.yaml
-
-zarf package create --tmpdir=/tmp --architecture amd64 --confirm
-zarf init --components=git-server --confirm
-zarf package deploy --confirm zarf-package-uds-idam-k3d-amd64-0.1.tar.zst
