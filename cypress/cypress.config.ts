dc462416c35311f198803916a6a09badd3c8c2b7
UnicornChance <chance@defenseunicorns.com>
2023-08-31 11:54:17 -0600

    fix: update tsconfig to use import

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     retries: 3,
     supportFile: 'support/e2e.ts',
